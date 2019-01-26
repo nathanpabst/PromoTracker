55dca0ed812630e6f8792c38798aeb709741e473
Nathan Pabst <nathan.pabst@gmail.com>
2019-01-26 15:01:40 -0600

    saved database to project folder and added code to return all promotions in the promoRepository class.

--- PromoTracker/DataAccess/PromoRepository.cs
+++ PromoTracker/DataAccess/PromoRepository.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper;
 using Microsoft.Extensions.Configuration;
+using PromoTracker.Models;
 
 namespace PromoTracker.DataAccess
 {
     public class PromoRepository
     {
+        static List<Promotion> _promotions = new List<Promotion>();
 
         private readonly string ConnectionString;
 
@@ -17,5 +20,14 @@ namespace PromoTracker.DataAccess
             ConnectionString = config.GetSection("ConnectionString").Value;
         }
 
+        public List<Promotion> GetPromotions()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var result = connection.Query<Promotion>("select * from Promotion");
+                return result.ToList();
+            }
+        }
     }
 }
