e4fa40e1f179066b1b70c13bd98961775cbc5691
Nathan Pabst <nathan.pabst@gmail.com>
2019-01-28 21:33:26 -0600

    components setup and navbar rendering

--- PromoTracker/ClientApp/src/App.js
+++ PromoTracker/ClientApp/src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import Navbar from './components/Navbar/Navbar';
 
-export default class App extends Component {
+class App extends React.Component {
   displayName = App.name
 
   render() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
-      </Layout>
+        <div className="App">
+            <Route>
+                <div>
+                    <Navbar />
+                </div>
+            </Route>
+        </div>
     );
   }
 }
+
+export default App;
---
+++ PromoTracker/ClientApp/src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+﻿import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Navbar.scss';
+
+class Navbar extends React.Component {
+    render() {
+
+    return (
+            <div className="Navbar navbar-fixed-top">
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <Link to="/" className="navbar-brand">Promo Code Tracker</Link>
+                    </div>
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        {
+                            <ul className="nav navbar-nav navbar-right">
+                                <li>
+                                    <Link to="/ingramspark" className="navbar-brand">IngramSpark</Link>
+                                </li>
+                                <li>
+                                    <Link to="/reporting" className="navbar-brand">Reporting</Link>
+                                </li>
+                                <li>
+                                    <Link to="/admin" className="navbar-brand">Admin</Link>
+                                </li>
+                            </ul>
+                        }
+                    </div>
+                </div>
+            </nav>
+          </div>
+          );
+
+    };
+}
+
+export default Navbar;
