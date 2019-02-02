adb7522ae49e342fcd510051f6dffb0c153eb82c
Nathan Pabst <nathan.pabst@gmail.com>
2019-02-02 10:44:33 -0600

    pulled model into a separate component. view on the ingramspark page is now working as expected.

---
+++ PromoTracker/ClientApp/src/components/Modals/PromoViewModal.js
@@ -0,0 +1,29 @@
+﻿import React, { Component } from 'react';
+import { Button, Modal, ModalHeader, ModalTitle, ModalBody, ModalFooter } from 'react-bootstrap';
+
+class PromoViewModal extends Component {
+    render() {
+        return (
+
+            <Modal show={this.props.show} onHide={this.props.hide}>
+                <ModalHeader closeButton>
+                    <ModalTitle>{this.props.promo.name}</ModalTitle>
+                </ModalHeader>
+                <ModalBody>
+                    <p>Start Date: {this.props.promo.start}</p>
+                    <br />
+                    <p>End Date: {this.props.promo.end}</p> <br />
+                    <p>Description: {this.props.promo.desc}</p> <br />
+                    <p>Restrictions: {this.props.promo.restrictions}</p> <br />
+                    <p>Category: {this.props.promo.category}</p>
+
+                </ModalBody>
+                <ModalFooter>
+                    <Button variant="secondary" onClick={this.props.hide}>Close</Button>
+                </ModalFooter>
+            </Modal>
+        );
+    }
+}
+
+export default PromoViewModal; 
