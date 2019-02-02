6ead9bbc02b89571190d8c11866b0555d267da20
Nathan Pabst <nathan.pabst@gmail.com>
2019-02-02 17:11:22 -0600

    orderRepository and orderController setup with SQL queries. need to test in Postman.

---
+++ PromoTracker/Controllers/OrderController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PromoTracker.DataAccess;
+using PromoTracker.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace PromoTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly OrderRepository _orders;
+
+        public OrderController(IConfiguration config)
+        {
+            _orders = new OrderRepository(config);
+        }
+
+        [HttpGet]
+        public IActionResult GetOrders()
+        {
+            var orderList = _orders.GetOrders();
+            return Ok(orderList);
+        }
+    }
+}
---
+++ PromoTracker/DataAccess/OrderRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using PromoTracker.Models;
+
+namespace PromoTracker.DataAccess
+{
+    public class OrderRepository
+    {
+        private readonly string ConnectionString;
+
+        public OrderRepository(IConfiguration config)
+        {
+            ConnectionString = config.GetSection("ConnectionString").Value;
+        }
+
+        public IEnumerable<Order> GetOrders()
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var result = connection.Query<Order>(@"SELECT * FROM [dbo].[order]");
+                return result;
+            }
+        }
+
+        public IEnumerable<Order> GetOrderById(int id)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var result = connection.Query<Order>(@"SELECT * 
+                                                        FROM [dbo].[order] 
+                                                        WHERE orderTypeId = @OrderTypeId", new { id });
+                return result;
+            }
+        }
+    }
+}
