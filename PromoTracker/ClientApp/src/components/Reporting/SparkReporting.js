d6ec480c26e9a28d691b6a605d58c309d98f0886
Nathan Pabst <nathan.pabst@gmail.com>
2019-02-06 21:16:16 -0600

    orders displaying on page as expected.

--- PromoTracker/ClientApp/src/components/Reporting/SparkReporting.js
+++ PromoTracker/ClientApp/src/components/Reporting/SparkReporting.js
@@ -10,20 +10,58 @@ class Reporting extends Component {
 
         this.state = {
             orders: []
-        }
+        };
     }
 
     componentDidMount() {
         OrderRequests
             .getOrders()
             .then(orders => {
-
+                this.setState({
+                    orders: orders.data
+                });
             })
+            .catch(error => console.log(error));
     }
+
     render() {
+        const { orders } = this.state;
+
+        const orderComponents = orders.map((order) => (
+            <tr key={order.id}>
+                <td>{order.orderNumber}</td>
+                <td>{order.bookId}</td>
+                <td>{order.orderDate}</td>
+                <td>{order.quantity}</td>
+                <td>{order.orderTypeId}</td>
+                <td>{order.printFee}</td>
+            </tr>
+
+        ));
         return (
             <div className="sparkReporting">
                 <h1>IngramSpark Reporting</h1>
+
+                <div className="panel panel-primary">
+                    <div className="panel-heading"> Orders </div>
+                    <div className="panel-body">
+                        <Table striped bordered hover size="sm">
+                            <thead>
+                                <tr>
+                                    <th>Order Number</th>
+                                    <th>Book Id</th>
+                                    <th>Order Date</th>
+                                    <th>Quantity</th>
+                                    <th>Order Type Id</th>
+                                    <th>Print Fees</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {orderComponents}
+                            </tbody>
+                        </Table>
+                    </div>
+                </div>
             </div>
 
             );
--- PromoTracker/ClientApp/src/components/Requests/OrderRequests.js
+++ PromoTracker/ClientApp/src/components/Requests/OrderRequests.js
@@ -1,7 +1,7 @@
 ﻿import axios from 'axios';
 
 const getOrders = () => {
-    const apiPath = `api/orders`;
+    const apiPath = `api/order`;
     return new Promise((resolve, reject) => {
         axios
             .get(apiPath)
