e828bd253341d3440d087979979f6e4eb330784d
Nathan Pabst <nathan.pabst@gmail.com>
2019-02-09 19:30:24 -0600

    chart component title displaying on Reporting page without errors.

--- PromoTracker/ClientApp/src/components/Reporting/SparkReporting.js
+++ PromoTracker/ClientApp/src/components/Reporting/SparkReporting.js
@@ -1,111 +1,54 @@
 ﻿import React, { Component } from 'react';
-import OrderRequests from './../Requests/OrderRequests';
-import { Table } from 'react-bootstrap';
-import Search from './../Search/Search';
+//import OrderRequests from './../Requests/OrderRequests';
+//import { Table } from 'react-bootstrap';
+//import Search from './../Search/Search';
+import ChartTitleCount from './../Charts/ChartTitleCount';
+import PromoRequests from '../Requests/PromoRequests';
 
 import './SparkReporting.css';
-import PromoRequests from '../Requests/PromoRequests';
 
 class Reporting extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            orders: [],
-            titles: [],
-            searchTerm: ''
+            chartData: props.chartData
         };
     }
+    componentWillMount() {
+        this.getChartData();
+    }  
 
-    componentDidMount() {
-        OrderRequests
-            .GetAggregatedOrderData()
-            .then(orders => {
-                this.setState({
-                    orders: orders.data
-                });
-            })
-            .catch(error => console.log(error)); 
-
+    getChartData() {
         PromoRequests
             .getTitleCountWithPromo()
-            .then(titles => {
+            .then(chartData => {
                 this.setState({
-                    titles: titles.data
+                    chartData: chartData.data
                 });
             })
             .catch(error => console.log(error));
     }
 
-
-    updateSearchInput = (searchTerm) => 
-        this.setState({searchTerm})
-    
-
     render() {
-        const { orders } = this.state;
 
-        const { titles } = this.state;
-
-        const orderComponents = orders.map((order, index) => (
-            <tr key={index}>
-                <td>{order.name}</td>
-                <td>{order.unitsShipped}</td>
-                <td>{order.printFees}</td>
-            </tr>
-        ));
-
-        const titleComponents = titles.map((title, index) => (
-            <tr key={index}>
-                <td>{title.promotionName}</td>
-                <td>{title.titleCount}</td>
-            </tr>
-        ));
-               
+        //const chartData = titles.map((title) =>
+        //    (return
+        //{
+        //    {
+        //        name: title.promotionName
+        //        value: title.titleCount
+        //    }
+        //}));
+             
         return (
-            <div className="sparkReporting">
-                <Search
-                    onSearch={this.updateSearchInput}
-                    searchTerm={this.state.searchTerm}
-                />                   
-
-                <div className="panel panel-primary">
-                    <div className="panel-heading">Orders</div>
-                    <div className="panel-body">
-                        <Table striped bordered hover size="sm">
-                            <thead>
-                                <tr>
-                                    <th>Promotion Name</th>
-                                    <th>Units Shipped</th>
-                                    <th>Print Fees</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {orderComponents}
-                            </tbody>
-                        </Table>
-                    </div>
-                </div>
-
-                <div className="panel panel-primary">
-                    <div className="panel-heading">Title Counts</div>
-                    <div className="panel-body">
-                        <Table striped bordered hover size="sm">
-                            <thead>
-                                <tr>
-                                    <th>Promotion Name</th>
-                                    <th>Title Additions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {titleComponents}
-                            </tbody>
-                        </Table>
-                    </div>
-                </div>
-                
+            <div className="sparkReporting">             
+                <div className="titleCountChart">
+                    <ChartTitleCount 
+                        chartData={this.state.chartData}
+                    />;
+                </div>                                                              
             </div>
-
             );
     }
 }
